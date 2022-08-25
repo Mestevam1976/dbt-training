39d070ca9ba7bf1f9711a9ac5a9e8f16a208ef48
Márcio Estevam da Silva <marcio.estevam@yahoo.com>
2022-08-25 17:58:41 +0000

    developing stage_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select
+--from raw orders
+o.orderid,
+o.orderdate,
+o.shipadate,
+o.shipmode,
+o.ordercostselling - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordercostselling,
+--from raw customer
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
